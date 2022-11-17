afe8db9039e68497ea7a6ef77dc34da814e6862c
CodeKaami <mdkamran1755@gmail.com>
2022-11-17 08:41:29 +0530

    completed navbar

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import "./styles/App.scss"
+import "./styles/header.scss"
+
+
+function App() {
+  return (
+    <Router >
+      <Header/>
+      <Routes>
+        <Route/>
+      </Routes>
+    </Router >
+  );
+}
+
+export default App;
